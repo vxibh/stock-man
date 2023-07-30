c6201a56da8f15767ec518b053273414c34d2c56
Vaibhav Sharma <vaibhav@192.168.1.4>
2023-07-30 15:11:32 +0530

    More Input Fields Added, Module Restrictions, Improved & Clean

--- stock-man-code.py
+++ stock-man-code.py
@@ -1,53 +1,74 @@
-import yfinance as yf #library for Stock Data
-import streamlit as st #StreamLit library
-from google_images_search import GoogleImagesSearch #Google Search Engine library
+import yfinance as yf
+import streamlit as st
+from google_images_search import GoogleImagesSearch
 
-st.write("""
-# Stock-Man 
-       
-Shown are the stock **closing price** and **volume** of the chosen company!
+# Function to fetch company logo URL using Google Images Search
+def get_company_logo_url(tickerSymbol):
+    search_params = {
+        'q': tickerSymbol + ' logo',
+        'num': 1,
+        'fileType': 'png',
+    }
+    gis = GoogleImagesSearch("AIzaSyC3sxaKXEtuibak-NeWtOdToLYgI3sWXTg", "41abd66df595642b6"
+)
+    gis.search(search_params=search_params)
+    results = gis.results()
+    if results:
+        return results[0].url
+    return None
 
-""")
+# Streamlit App
+def main():
+    st.write("""
+    # Stock-Man
 
-# Defining ticker symbol
-tickerSymbol = st.text_input("Enter the company's name (e.g., TSLA):")
+    Shown are the stock **closing price** and **volume** of the chosen company!
+    """)
 
-if st.button("Show Graphs"):
-    # Getting data
-    tickerData = yf.Ticker(tickerSymbol)
+    # Input fields
+    tickerSymbol = st.text_input("Enter the company's name (e.g., TSLA):")
+    start_date = st.date_input("Enter the start date:")
+    end_date = st.date_input("Enter the end date:")
+    period = st.selectbox("Select the period", ["1d", "1wk", "1mo"])
 
-    # Checking if the input is not empty and the data is available
-    if tickerSymbol and tickerData:
-        # retrieve data
-        tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+    # Show graphs button
+    if st.button("Show Graphs"):
+        if not tickerSymbol:
+            st.warning("Please enter a valid company name.")
+            return
 
-        # Searching logo for the company
-        company_logo = {
-            'q': tickerSymbol + ' logo',  # Search query using the company name and "logo" keyword
-            'num': 1,                      # Number of images to fetch
-            'fileType': 'png',
-        }
+        # Getting data
+        tickerData = yf.Ticker(tickerSymbol)
+        if not tickerData:
+            st.error("Data not available for the provided company.")
+            return
 
-        # Perform Google image search for the company's logo
-        gis = GoogleImagesSearch("AIzaSyC3sxaKXEtuibak-NeWtOdToLYgI3sWXTg", "41abd66df595642b6")
-        gis.search(search_params=company_logo)
-        image_result = gis.results()[0].url
+        # Retrieve data
+        tickerDf = tickerData.history(period=period, start=start_date, end=end_date)
 
-        # Displaying the company logo and name using the fetched image
-        st.markdown(
-            f'<img src="{image_result}" alt="{tickerSymbol} logo" style="width: 150px;">',
-            unsafe_allow_html=True
-        )
-        
-        #closing graph
+        # Fetch company logo URL
+        company_logo_url = get_company_logo_url(tickerSymbol)
+
+        if company_logo_url:
+            # Display company logo and name
+            st.image(company_logo_url, caption=tickerSymbol + ' logo', use_column_width=True)
+
+        # Closing price graph
         st.write("""
         ## Closing Price
         """)
         st.line_chart(tickerDf.Close)
 
-        #volume graph
+        # Volume graph
         st.write("""
         ## Volume Price
         """)
         st.line_chart(tickerDf.Volume)
 
+        # Major Holders information
+        st.write("""## Major Holders""")
+        st.write(tickerData.major_holders)
+
+#for only code run
+if __name__ == '__main__':
+    main()
